950d4590e82657019204f9a0be11bf5042be096e
muke <muke101@protonmail.com>
2021-09-28 18:33:59 +0100

    implemented map_block_line_numbers

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::process::Command;
 use std::path::Path;
+use std::io::{self, BufReader, BufRead};
+use std::fs::{File, read_to_string};
 use inkwell::module::Module;
 use inkwell::context::Context;
 use inkwell::values::{BasicValueEnum, InstructionValue};
 use inkwell::basic_block::BasicBlock;
 use either::Either;
+use std::collections::HashMap;
+use regex::Regex;
 
 fn get_test_ir(ctx: &Context) -> Module  {
     Command::new("llvm-as").arg("test.ll");
@@ -18,20 +22,48 @@ fn get_first_inst<'a>(module: &'a Module) -> InstructionValue<'a> {
     return entry.get_first_instruction().unwrap();
 }
 
-//FIXME: does this mess with ownership?
 fn get_defs<'a>(inst: &'a InstructionValue) -> Vec<Either<BasicValueEnum<'a>, BasicBlock<'a>>>   {
-    return (0..inst.get_num_operands())
-            .map(|i| inst.get_operand(i)
-                 .unwrap())
-            .collect::<Vec<_>>();
+    return (0..inst.get_num_operands()).map(|i| inst.get_operand(i)
+                                            .unwrap())
+                                       .collect::<Vec<_>>();
 }
 
-/*iterate over lines,
- * map ssa names to line numbers,
- * iterate over functions,
- * basic blocks and instructions,
- * get their ssa names,
- * remap lines to objects*/
+fn get_lines() -> Vec<String>  {
+    let path = Path::new("./test.ll");
+    let raw = read_to_string(path).unwrap();
+    return raw.lines()
+              .map(String::from)
+              .collect::<Vec<_>>();
+}
+
+fn map_block_line_numbers(lines: &Vec<String>) -> HashMap<&str, u32>  {
+    let mut block_lines: HashMap<&str, u32> = HashMap::new();
+
+    let block = Regex::new(r"^.:\n$").unwrap();
+    for (i, line) in lines.iter().enumerate()   {
+        if block.is_match(line) {
+            let len = line.len();
+            let mut a = 0;
+            let mut b = len-3;
+            match line.find(" ")    {
+                Some(_) => {a += 1; b -= 1;},
+                None => (),
+            };
+            block_lines.insert(&line[a..b], i as u32);
+        }
+    }
+
+    return block_lines;
+}
+
+/*
+ * iterate over lines, map function and basic block names to line numbers
+ * map names to objects
+ * iterate through lines, for each basic block find it's object
+ * iterate through instructions of basic block object
+ * map instructions to line numbers after basic block sequentially
+ * save their names
+ */
 
 fn main() {
     let ctx = Context::create();
